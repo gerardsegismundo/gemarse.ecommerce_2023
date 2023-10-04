b3f3d21612b74ff2e8f40cc5ba406fc2c8097ebb
gerardsegismundo <gerardmartinsegismundo@protonmail.com>
2023-10-04 10:55:36 -0700

    add trending images

---
+++ client/src/assets/data/trendingData.js
@@ -0,0 +1,23 @@
+import stripeShirt from '../images/trending_items/stripe-shirt.jpg'
+import leggings from '../images/trending_items/leggings.jpg'
+
+const trendingData = [
+  {
+    id: 8,
+    imgSrc: leggings,
+    color: 'black',
+    price: 58.99,
+    category: 'womens',
+    type: 'leggings'
+  },
+  {
+    id: 14,
+    imgSrc: stripeShirt,
+    color: 'black',
+    price: 39.99,
+    category: 'mens',
+    type: 'shirt'
+  }
+]
+
+export default trendingData

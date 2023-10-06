48388e1eece5a45eb17c4221b5c298aa6e189e89
gerardsegismundo <gerardmartinsegismundo@protonmail.com>
2023-10-05 18:04:06 -0700

    Add story images'

--- client/src/components/Story.jsx
+++ client/src/components/Story.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
 
+import AboutUs from '../assets/images/story/about-us.jpg'
+import ContactUs from '../assets/images/story/contact.jpg'
+import DeliveriesReturn from '../assets/images/story/deliveries-return.jpg'
+import FAQs from '../assets/images/story/FAQs.jpg'
+import GiftCards from '../assets/images/story/gift-cards.jpg'
+
 const Story = () => {
-  return <div>Story</div>
+  return (
+    <div className='story'>
+      <h3>Read the Gemarse story</h3>
+      <div className='gallery-grid'>
+        <img src={AboutUs} alt='about us' />
+        <img src={ContactUs} alt='contact us' />
+        <img src={DeliveriesReturn} alt='deliveries return' />
+        <img src={FAQs} alt='FAQs' />
+        <img src={GiftCards} alt='gift cards' />
+      </div>
+    </div>
+  )
 }
 
 export default Story

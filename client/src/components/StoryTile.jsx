ca19cff10b85d1c9c410d01d8fc174cd7e764ad2
gerardsegismundo <gerardmartinsegismundo@protonmail.com>
2023-10-09 12:38:20 -0700

    Story hover features added.

--- client/src/components/StoryTile.jsx
+++ client/src/components/StoryTile.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { ReactComponent as ChevronRightIcon } from '../assets/svg/chev-right.svg'
 
 const StoryTile = ({ to, imgSrc, message, title, className }) => {
   return (
     <Link to={to}>
       <img src={imgSrc} alt='about us' />
       <p>{message}</p>
-      <h4>{title}</h4>
+      <h4>
+        <ChevronRightIcon />
+        {title}
+      </h4>
     </Link>
   )
 }

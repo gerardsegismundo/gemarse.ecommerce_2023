95eaf8aee13156313980c3a914ca63261ad6daef
gerardsegismundo <gerardmartinsegismundo@protonmail.com>
2023-10-10 21:41:28 -0700

    Product list item added.

--- client/src/components/SubMenu.jsx
+++ client/src/components/SubMenu.jsx
@@ -1,20 +1,20 @@
 import React, { useState } from 'react'
 import { ReactComponent as SearchIcon } from '../assets/svg/search.svg'
 
-const MensShopByProductLists = [
+const MensShopByProductListItems = [
   { label: 'Shirts & Polos', url: '' },
-  { label: '', url: '' },
-  { label: '', url: '' },
-  { label: '', url: '' },
-  { label: '', url: '' },
-  { label: '', url: '' },
-  { label: '', url: '' },
-  { label: '', url: '' },
-  { label: '', url: '' },
-  { label: '', url: '' },
-  { label: '', url: '' },
-  { label: '', url: '' },
-  { label: '', url: '' }
+  { label: 'Tees', url: '' },
+  { label: 'Hoodies', url: '' },
+  { label: 'Sweaters', url: '' },
+  { label: 'Pants & Chinos', url: '' },
+  { label: 'Joggers', url: '' },
+  { label: 'Shorts & Swimwear', url: '' },
+  { label: 'Coats & Jackets', url: '' },
+  { label: 'Activewear', url: '' },
+  { label: 'Underwear', url: '' },
+  { label: 'Accessories', url: '' },
+  { label: 'Coming Soon', url: '' },
+  { label: 'ViewList', url: '' }
 ]
 
 const SubMenu = () => {

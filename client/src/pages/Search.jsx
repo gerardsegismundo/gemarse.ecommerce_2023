c7e17bea45affbef57c2fd2792dd62303e133624
gerardsegismundo <gerardmartinsegismundo@protonmail.com>
2023-10-24 20:30:21 -0700

    search result

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,17 +1,40 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
-import slugify from '../utils/helpers/slugify'
 import { ReactComponent as SearchIcon } from '../assets/svg/search.svg'
+import products from '../assets/data/products'
+import ProductCard from '../components/ProductCard'
 
 const Search = () => {
   const { search } = useLocation()
   const queryParams = new URLSearchParams(search)
   const searchQuery = queryParams.get('q')
+  const [searchResult, setSearchResult] = useState('')
 
   const [searchVal, setSearchVal] = useState('')
 
-  const handleOnSearch = () => {
-    const sluggifiedSearchVal = slugify(searchVal)
+  const handleOnSearch = product => {
+    const productWithoutSpaces = product.replace(/\s/g, '') // Remove spaces
+    const regex = new RegExp(productWithoutSpaces, 'gi')
+
+    const matchingProducts = products.filter(product => {
+      return (
+        regex.test(product.name.replace(/\s/g, '')) ||
+        regex.test(product.name) ||
+        regex.test(product.type.replace(/\s/g, '')) ||
+        regex.test(product.type) ||
+        regex.test(product.category.replace(/\s/g, '')) ||
+        regex.test(product.category) ||
+        regex.test(product.color.replace(/\s/g, '')) ||
+        regex.test(product.color)
+      )
+    })
+
+    if (matchingProducts.length > 0) {
+      console.log({ matchingProducts })
+      setSearchResult(matchingProducts)
+    } else {
+      console.log('NOT FOUND')
+    }
   }
 
   const handleOnChange = e => setSearchVal(e.target.value)
@@ -20,14 +43,24 @@ const Search = () => {
     if (e.key === 'Enter') handleOnSearch()
   }
 
+  useEffect(() => {
+    if (searchQuery) {
+      handleOnSearch(searchQuery)
+    }
+  }, [searchQuery])
+
   return (
     <div className='search-page'>
       <div className='container'>
         <div className='header-group'>
-          <h1>Your search for "{searchQuery}" did not yield any results.</h1>
+          <h1>
+            {searchResult
+              ? `Your search for "${searchQuery}" revealed the following:`
+              : `Your search for "${searchQuery}" did not yield any results.`}
+          </h1>
           <form className='search-group' onSubmit={e => e.preventDefault()}>
             <button onClick={handleOnSearch}>
-              <SearchIcon />
+              <SearchIcon onClick={() => console.log('CLICKED!')} />
             </button>
             <input
               type='search'
@@ -39,6 +72,7 @@ const Search = () => {
             />
           </form>
         </div>
+        {searchResult && searchResult.map(data => <ProductCard props={data} key={data._id} />)}
       </div>
     </div>
   )

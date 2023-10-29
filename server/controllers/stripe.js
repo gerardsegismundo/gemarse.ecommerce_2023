15edb8e3204f261e356a5d554fee68f4e2577e22
gerardsegismundo <gerardmartinsegismundo@protonmail.com>
2023-10-29 10:56:00 -0700

    deploy

--- server/controllers/stripe.js
+++ server/controllers/stripe.js
@@ -3,7 +3,7 @@ import Stripe from 'stripe'
 
 const stripe = Stripe(process.env.STRIPE_KEY)
 
-const stripeCheckout = asyncHandler(async (req, res) => {
+const stripeCheckout = async (req, res) => {
   const line_items = req.body.cartItems.map(item => {
     return {
       price_data: {
@@ -24,63 +24,65 @@ const stripeCheckout = asyncHandler(async (req, res) => {
     }
   })
 
-  // const session = await stripe.checkout.sessions.create({
-  //   shipping_address_collection: {
-  //     allowed_countries: ['US', 'CA']
-  //   },
-  //   shipping_options: [
-  //     {
-  //       shipping_rate_data: {
-  //         type: 'fixed_amount',
-  //         fixed_amount: {
-  //           amount: 0,
-  //           currency: 'usd'
-  //         },
-  //         display_name: 'Free shipping',
-  //         delivery_estimate: {
-  //           minimum: {
-  //             unit: 'business_day',
-  //             value: 5
-  //           },
-  //           maximum: {
-  //             unit: 'business_day',
-  //             value: 7
-  //           }
-  //         }
-  //       }
-  //     },
-  //     {
-  //       shipping_rate_data: {
-  //         type: 'fixed_amount',
-  //         fixed_amount: {
-  //           amount: 0,
-  //           currency: 'usd'
-  //         },
-  //         display_name: 'Next day air',
-  //         delivery_estimate: {
-  //           minimum: {
-  //             unit: 'business_day',
-  //             value: 1
-  //           },
-  //           maximum: {
-  //             unit: 'business_day',
-  //             value: 1
-  //           }
-  //         }
-  //       }
-  //     }
-  //   ],
-  //   phone_number_collection: {
-  //     enabled: true
-  //   },
-  //   line_items,
-  //   mode: 'payment',
-  //   success_url: `${process.env.CLIENT_URL}/checkout-success`,
-  //   cancel_url: `${process.env.CLIENT_URL}/cart`
-  // })
+  const session = await stripe.checkout.sessions.create({
+    shipping_address_collection: {
+      allowed_countries: ['US', 'CA']
+    },
+    shipping_options: [
+      {
+        shipping_rate_data: {
+          type: 'fixed_amount',
+          fixed_amount: {
+            amount: 0,
+            currency: 'usd'
+          },
+          display_name: 'Free shipping',
+          delivery_estimate: {
+            minimum: {
+              unit: 'business_day',
+              value: 5
+            },
+            maximum: {
+              unit: 'business_day',
+              value: 7
+            }
+          }
+        }
+      },
+      {
+        shipping_rate_data: {
+          type: 'fixed_amount',
+          fixed_amount: {
+            amount: 0,
+            currency: 'usd'
+          },
+          display_name: 'Next day air',
+          delivery_estimate: {
+            minimum: {
+              unit: 'business_day',
+              value: 1
+            },
+            maximum: {
+              unit: 'business_day',
+              value: 1
+            }
+          }
+        }
+      }
+    ],
+    phone_number_collection: {
+      enabled: true
+    },
+    line_items,
+    mode: 'payment'
 
-  // res.send({ url: session.url })
-  res.send({ line_items })
-})
+    // success_url: `${process.env.CLIENT_URL}/checkout-success`,
+    // cancel_url: `${process.env.CLIENT_URL}/`
+  })
+
+  window.location.href = process.env.CLIENT_URL
+
+  res.send({ paymentSuccessful: true })
+}
 
 export { stripeCheckout }

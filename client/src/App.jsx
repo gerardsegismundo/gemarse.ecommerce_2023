abd13ed2db0ee64fe6b94e86fae9344a7dd6b816
gerardsegismundo <gerardmartinsegismundo@protonmail.com>
2023-10-29 10:08:18 -0700

    deploy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import { Route, Routes } from 'react-router'
-import { Home, Mens, Womens, Accessories, Account, Search, Cart } from './pages'
+import { Home, Mens, Womens, Accessories, Account, Search, Cart, CheckoutSucces } from './pages'
 import Header from './components/Headers'
 import { CartDrawer, Footer, SubMenu, RecentlyViewed, Product } from './components'
 import { useLocation } from 'react-router-dom'
@@ -23,6 +23,7 @@ const App = () => {
       <Routes>
         <Route exact={true} path='/' element={<Home />} />
         <Route exact={true} path='/account' element={<Account />} />
+        <Route path='/checkout-success' element={<CheckoutSucces />} />
         <Route exact={true} path='/mens' element={<Mens />} />
         <Route exact={true} path='/womens' element={<Womens />} />
         <Route exact={true} path='/accessories' element={<Accessories />} />
---
+++ client/src/pages/CheckoutSuccess.jsx
@@ -0,0 +1,9 @@
+const CheckoutSuccess = () => {
+  return (
+    <div className='checkout-success-page'>
+      <h2>CheckoutSuccess</h2>
+    </div>
+  )
+}
+
+export default CheckoutSuccess
